38b5c5d4476a91913e3a55b929f39cac1b93d902
Jose Melero Martinez <jmelero08@gmail.com>
2021-07-19 16:47:40 -0600

    added post request

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const endPoint = "http://localhost:3000/api/v1/workout_plans"
 
 document.addEventListener('DOMContentLoaded', () => {
     getWorkouts()
+
+    const createWorkoutPlanForm = document.querySelector('#create-workoutplan-form')
+    createWorkoutPlanForm.addEventListener("submit", (e) => createFormHandler(e))
+
 })
 
 function getWorkouts() {
@@ -21,5 +25,38 @@ function getWorkouts() {
           document.querySelector('#workout-container').innerHTML += workoutMarkup
       })
     })
+}
+
+function createFormHandler(e) {
+    e.preventDefault()
+    const titleInput = document.querySelector('#input-title').value
+    const descriptionInput = document.querySelector('#input-description').value
+    const imageInput = document.querySelector('#input-url').value
+    const categoryId = parseInt(document.querySelector('#categories').value)
+    postFetch(titleInput, descriptionInput, imageInput, categoryId)
+}
+
+function postFetch(title, description, image_url, category_id){
+    const bodyData = {title, description, image_url, category_id}
+    fetch(endPoint, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(bodyData)
+    })
+    .then(response => response.json())
+    .then(workout => {
+        const workoutData = workout.data.attributes
+        // render JSON response
+        const workoutMarkup = `
+        <div data-id=${workout.id}>
+          <img src=${workoutData.attributes.image_url} height="200" width="250">
+          <h3>${workoutData.attributes.title}</h3>
+          <p>${workoutData.attributes.category.name}</p>
+          <button data-id=${workoutData.id}>edit</button>
+        </div>
+        <br><br>`;
+    
+        document.querySelector('#workout-container').innerHTML += workoutMarkup;
+      })
 }
 
